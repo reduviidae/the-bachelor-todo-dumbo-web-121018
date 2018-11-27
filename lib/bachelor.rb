19c43105b18c043f9c2d0a8f4b8930c870a2b8c4
Wil Cook <wil.cook.is@gmail.com>
2018-11-27 19:23:06 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,67 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  data.map do |key, value|
+    if key == season
+      value.map do |contestants|
+        if contestants["status"] == "Winner"
+        winner_full = contestants["name"].split(" ")
+        winner = winner_full[0]
+        end
+      end
+    end
+  end
+  winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = ""
+  data.map do |key, value|
+    value.map do |contestants|
+      if contestants["occupation"] == occupation
+        name = contestants["name"]
+      end
+    end
+  end
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i = 0
+  data.map do |key, value|
+    value.map do |contestants|
+      if contestants["hometown"] == hometown
+        i += 1  
+      end
+    end
+  end
+  i
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation = ""
+  data.map do |key, value|
+    value.map do |contestants|
+      if contestants["hometown"] == hometown
+        return occupation = contestants["occupation"]
+      end
+    end
+  end
+  occupation
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  count = 0
+  total_age = 0
+  data.map do |key, value|
+    value.map do |contestants|
+      if key == season
+        total_age += contestants["age"].to_i
+        count += 1  
+      end
+    end
+  end
+  # binding.pry
+  average_age = (total_age / count)
 end
