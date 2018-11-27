34946e8d7ef5b2ca681e2919476751cbcbd41a50
Wil Cook <wil.cook.is@gmail.com>
2018-11-27 21:21:09 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -58,7 +58,7 @@ def get_average_age_for_season(data, season)
   data[season].map do |contestants|
     unless contestants["age"] == ""
       # binding.pry
-      total_age += contestants["age"].to_i
+      total_age += contestants["age"].to_f
       count += 1 
     end
   end
